0efef48eb0c8a8f76a32628dbf3de451ff79907c
Finn <mailtorufina@gmail.com>
2023-05-17 13:51:51 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header />
     </div>
   );
 }
