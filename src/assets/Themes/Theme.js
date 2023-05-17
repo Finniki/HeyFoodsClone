906c1edff81bc49c49e13f1b7e49b096050cd2f0
Finn <mailtorufina@gmail.com>
2023-05-17 14:14:25 +0100

    add color theme styling

--- src/assets/Themes/Theme.js
+++ src/assets/Themes/Theme.js
@@ -1,1 +1,17 @@
 import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  status: {
+    danger: "#e53e3e",
+  },
+  palette: {
+    primary: {
+      main: "#000",
+      darker: "#000",
+    },
+    neutral: {
+      main: "#64748B",
+      contrastText: "#fff",
+    },
+  },
+});
