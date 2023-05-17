3b2820dfaec8f133a56d02fc27df3c30697bc4ab
Finn <mailtorufina@gmail.com>
2023-05-17 13:29:56 +0100

    write code for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,23 @@
 import styles from "./Header.module.css";
-import Grid from "@mui/material/Unstable_Grid2";
 
 const Header = () => {
-    return (
-
-    )
-}
+  return (
+    <div>
+      <header class={styles.header}>
+        <div class={styles.brand}>
+          <img
+            src="./images/heyfoodlogo2.png"
+            class={styles.brandLogo}
+            alt="HeyFoods logo"
+          />
+        </div>
+        <nav class={styles.signBtns}>
+          {/* <button class={`${styles.signIn} ${styles.button}`}>Sign In</button>
+          <button class={`${styles.signUp} ${styles.button}`}>Sign Up</button> */}
+        </nav>
+      </header>
+    </div>
+  );
+};
 
 export default Header;
