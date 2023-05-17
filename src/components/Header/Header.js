36c975be7075321e9a77893811caedbcfb8c745f
Finn <mailtorufina@gmail.com>
2023-05-17 13:38:47 +0100

    import Button component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,13 @@
 import styles from "./Header.module.css";
 import logo from "../../assets/images/heyfoodlogo2.png";
+import { Button } from "@mui/material";
 
 const Header = () => {
   return (
     <div>
       <header class={styles.header}>
         <div class={styles.brand}>
-          <img
-            src="./images/heyfoodlogo2.png"
-            class={styles.brandLogo}
-            alt="HeyFoods logo"
-          />
+          <img src={logo} class={styles.brandLogo} alt="HeyFoods logo" />
         </div>
         <nav class={styles.signBtns}>
           {/* <button class={`${styles.signIn} ${styles.button}`}>Sign In</button>
