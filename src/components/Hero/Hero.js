bb4c98ce03a917c2a7be3a744837bb372618fb25
Finn <mailtorufina@gmail.com>
2023-05-19 15:59:11 +0100

    add text and image divs

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,12 @@
 import styles from "./Hero.module.css";
 
 const Hero = () => {
-    return (
-        
-    )
-}
+  return (
+    <section>
+      <div className={styles.textArea}></div>
+      <div className={styles.imageArea}></div>
+    </section>
+  );
+};
 
 export default Hero;
