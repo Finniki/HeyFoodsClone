1cf369fb2e2a11afbf8bd6ec78d3ec5964b54f61
Finn <mailtorufina@gmail.com>
2023-05-19 16:01:48 +0100

    add Hero section text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,11 @@ import styles from "./Hero.module.css";
 const Hero = () => {
   return (
     <section>
-      <div className={styles.textArea}></div>
+      <div className={styles.textArea}>
+        <h1>
+          Your food<span>delivered</span> within minutes
+        </h1>
+      </div>
       <div className={styles.imageArea}></div>
     </section>
   );
