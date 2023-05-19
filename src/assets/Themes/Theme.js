77f224caa96c67b110e1ee26b17b677c0e4123e5
Finn <mailtorufina@gmail.com>
2023-05-19 12:01:55 +0100

    edit theme colors

--- src/assets/Themes/Theme.js
+++ src/assets/Themes/Theme.js
@@ -1,17 +1,14 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  status: {
-    danger: "#e53e3e",
-  },
   palette: {
     primary: {
-      main: "#000",
-      darker: "#000",
+      main: "#000000",
+      contrastText: "#ffffff",
     },
-    neutral: {
-      main: "#64748B",
-      contrastText: "#fff",
+    secondary: {
+      main: "#dddddd",
+      contrastText: "#000000",
     },
   },
 });
