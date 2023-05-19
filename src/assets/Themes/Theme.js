dc4758c815a5cbbbf7b0eb22e17a1619108e9241
Finn <mailtorufina@gmail.com>
2023-05-19 14:49:35 +0100

    set button color styling

--- src/assets/Themes/Theme.js
+++ src/assets/Themes/Theme.js
@@ -4,10 +4,12 @@ const theme = createTheme({
   palette: {
     primary: {
       main: "#000000",
+      light: "#d7d7d7",
+      dark: "#333333",
       contrastText: "#ffffff",
     },
     secondary: {
-      main: "#dddddd",
+      main: "#ffffff",
       contrastText: "#000000",
     },
   },
