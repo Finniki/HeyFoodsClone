0d063ea4a724e850507945b33787770fcca3f4f8
Finn <mailtorufina@gmail.com>
2023-05-20 23:59:38 +0100

    import button styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import logo from "../../assets/images/heyfoodlogo2.png";
 import { Button } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import theme from "../../assets/Themes/Theme";
+import buttonStyles from "../../assets/ButtonStyle/ButtonStyle";
 
 const Header = () => {
   return (
